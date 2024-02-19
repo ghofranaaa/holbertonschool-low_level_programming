8a527b56e3200151bc649c581bb595d5ddd37e7d
ghofranaaa <smailikhaled841@gmail.com>
2024-02-19 21:06:17 +0100

    size of oprator

---
+++ hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints the sizes of different data types.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+
+{
+printf("size of a char is: %lu\n", sizeof(char));
+printf("size of an int is: %lu\n", sizeof(int));
+printf("size of a float is: %lu\n", sizeof(float));
+printf("size of a long int is: %lu\n", sizeof(long int));
+
+printf("size of a double is: %lu\n", sizeof(double));
+return (0);
+}
