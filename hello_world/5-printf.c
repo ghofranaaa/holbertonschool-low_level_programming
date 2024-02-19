239f8701d7eb4a4747b0ab2890711e88637931b7
ghofranaaa <smailikhaled841@gmail.com>
2024-02-19 20:08:15 +0100

    printf

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+
+
 /**
  * main - Prints "printing, followed by a new line.
  *
@@ -8,6 +10,7 @@
 int main(void)
 
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art");
+
 	return (0);
 }
