8afa18187a0e1f4be78a9af4e5555053872545b0
ghofranaaa <smailikhaled841@gmail.com>
2024-02-20 11:10:30 +0100

    size of oprator

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -8,13 +8,11 @@
  */
 
 int main(void)
-
 {
 printf("size of a char is: %lu\n", sizeof(char));
 printf("size of an int is: %lu\n", sizeof(int));
 printf("size of a float is: %lu\n", sizeof(float));
 printf("size of a long int is: %lu\n", sizeof(long int));
-
 printf("size of a double is: %lu\n", sizeof(double));
 return (0);
 }
