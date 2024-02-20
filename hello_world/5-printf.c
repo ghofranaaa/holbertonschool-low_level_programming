3a26a87b9dc83d92840763c409a3d1de1f9d3036
ghofranaaa <smailikhaled841@gmail.com>
2024-02-20 11:06:45 +0100

    size of oprator

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-
-
 /**
  * main - Prints "printing, followed by a new line.
  *
@@ -8,9 +6,7 @@
  */
 
 int main(void)
-
 {
-	printf("with proper grammar, but the outcome is a piece of art");
-printf("\n");
-	return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
