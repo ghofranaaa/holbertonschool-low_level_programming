5501185db044ce123e3bff9571d575d1f566daa1
ghofranaaa <smailikhaled841@gmail.com>
2024-02-21 23:22:26 +0100

    single digits

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - Entry point of the program
+*
+* Description: print all single digit numbers, followed by a line.
+*
+* Return: Always 0.
+**/
+int main(void)
+{
+int x;
+
+for (x = 0; x < 10; x++)
+{
+printf("%d", x);
+}
+return (0);
+}
