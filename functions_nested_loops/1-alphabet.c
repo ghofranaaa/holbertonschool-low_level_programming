ae6b50997c75dc4800f48c182db2c0469568ec93
ghofranaaa <smailikhaled841@gmail.com>
2024-02-24 14:48:28 +0100

    print_alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* print_alphabet - Printing the alphabet in lowercase, followed by a new line.
+*/
+void print_alphabet(void)
+{
+char letter = 'a';
+
+while (letter <= 'z')
+{
+_putchar(letter);
+letter++;
+}
+_putchar('\n');
+}
