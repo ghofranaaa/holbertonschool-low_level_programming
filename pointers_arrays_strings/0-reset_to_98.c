7e5a7faf32adc0cf030faf93156663d048073053
ghofranaaa <smailikhaled841@gmail.com>
2024-02-28 10:46:50 +0100

    reset to 98

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - takes pointer to a data type as parameter
+ * and updates the value of it.
+ * @n: integer.
+ * Description: pointer to an int as parameter updates its value.
+*/
+void reset_to_98(int *n)
+{
+*n = 98;
+}
