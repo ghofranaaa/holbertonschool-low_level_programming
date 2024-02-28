37cf6a420a9eea4e707e58b5b08e7f02532dfad2
ghofranaaa <smailikhaled841@gmail.com>
2024-02-28 14:42:53 +0100

    printing a string in reverse

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -8,13 +8,17 @@
 void print_rev(char *s)
 {
 int i, j;
-j = strlen(s);
+
+j = 0;
+while (s[j] != '\0')
+{
+	j++;
+}
 
 for (i = j - 1; i >= 0; i--)
 {
-	_putchar(s[i]);
+_putchar(s[i]);
 }
 
 _putchar('\n');
-
 }
