2275e117168f6658faa534326afcc666c987c1e2
ghofranaaa <smailikhaled841@gmail.com>
2024-03-05 09:50:51 +0100

    searching a string for any bytes

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,29 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strpbrk - a function that gets the length of a prefix substring.
+ * @s: first string.
+ * @accept: second string.
+ * Return: Returns a pointer to the bytes in s that matches one of the byte
+ * in accept, or null if no such byte is found.
+ */
+
+
+
+
+char *_strpbrk(char *s, char *accept)
+{
+int i;
+while (*s)
+{
+	for (i = 0; accept[i]; i++)
+	{
+	if (*s == accept[i])
+	return (s);
+	}
+	s++;
+}
+return (NULL);
+
+}
