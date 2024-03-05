cecad4fb6dbc8312a0094e9a053755fa2a0a49ea
ghofranaaa <smailikhaled841@gmail.com>
2024-03-05 17:27:22 +0100

    printing a string with puts

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _puts_recursion - a function that prints a string.
+ * @s: pointer to a string.
+ * Description: printing a string, followed by a new line.
+ */
+
+
+
+void _puts_recursion(char *s)
+{
+
+puts(s);
+
+}
