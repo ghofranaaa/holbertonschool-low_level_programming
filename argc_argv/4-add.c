3f2e076673ecb2940946c941ab734526e1e180d2
ghofranaaa <smailikhaled841@gmail.com>
2024-03-09 23:08:56 +0100

    a program that adds two numbers

---
+++ argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - a program that multiplies two numbers,
+ * followed by a new line.
+ * @argc: argument count.
+ * @argv: argument vector.
+ * Description: a program that multiplies two numbers.
+ * Return: 0, otherwise 1 .
+ **/
+
+int main(int argc, char **argv)
+{
+	char *s;
+	int n = 0;
+
+	while (--argc)
+	{
+		for (s = argv[argc]; *s; s++)
+		{
+			if (*s < '0' || *s > '9')
+				printf("Error\n");
+			return (1);
+		}
+		n = n + (atoi(argv[argc]));
+	}
+	printf("%d\n", n);
+	return (0);
+}
