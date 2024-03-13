4cbbb3c79943263f561b8b6382dcdb5174bf399d
ghofranaaa <smailikhaled841@gmail.com>
2024-03-13 17:58:34 +0100

    allocating space in memory for 2d array grid

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,53 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * **alloc_grid - a function that returns a pointer to a 2 dimensional
+ * array of integers.
+ * @width: the width of the grid.
+ * @height: the height of the grid.
+ *
+ * Return: pointer to a 2 dimensional array of integers,
+ * or null if it fails.
+ */
+
+int **alloc_grid(int width, int height)
+{
+
+	int **p;
+	int i;
+	int j;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	p = malloc(sizeof(int) * height);
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		p[i] = malloc(sizeof(int) * width);
+		if (p[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+			{
+				free(p[j]);
+			}
+			free(p);
+			return (NULL);
+		}
+	}
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			p[i][j] = 0;
+		}
+	}
+	return (p);
+}
