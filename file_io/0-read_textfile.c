2b256424cf5775d1ded106f93d51f897840a0152
ghofranaaa <smailikhaled841@gmail.com>
2024-04-06 18:23:46 +0100

    reading a text file and printing it to stdout

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,53 +1,58 @@
 #include "main.h"
-#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
 #include <stdlib.h>
-/**
- * read_textfile - a function that reads a text file and prints it .
- * @filename: The path to the file.
- * @letters: The number of letters.
- *
- * Return: returns the actual number of letters it could read and print.
- */
+	/**
+	 * read_textfile - a function that reads a text file and prints it .
+	 * @filename: The path to the file.
+	 * @letters: The number of letters.
+	 *
+	 * Return: returns the actual number of letters it could read and print.
+	 */
 
-ssize_t read_textfile(const char *filename, size_t letters)
+	ssize_t
+	read_textfile(const char *filename, size_t letters)
 {
-	FILE *f;
+	int f;
 	char *b;
-	size_t b_r;
-	size_t b_w;
-if (filename == NULL)
-{
-	return (0);
-}
-f = fopen(filename, "r");
-if (f == NULL)
-{
-	return (0);
-}
-b = (char *)malloc(sizeof(char) * (letters + 1));
-if (b == NULL)
-{
-	fclose(f);
-	free(b);
-	return (0);
-}
-b_r = fread(b, sizeof(char), letters, f);
-if (b_r == 0)
-{
-	fclose(f);
-	free(b);
-	return (0);
-}
+	ssize_t b_r;
+	ssize_t b_w;
 
-b[b_r] = '\0';
-b_w = fwrite(b, sizeof(char), b_r, stdout);
-if (b_w != b_r)
-{
-	fclose(f);
-	free(b);
-	return (0);
-}
-	fclose(f);
+	if (filename == NULL)
+	{
+		return (0);
+	}
+
+	f = open(filename, O_RDONLY);
+	if (f == -1)
+	{
+		return (0);
+	}
+
+	b = malloc(sizeof(char) * letters);
+	if (b == NULL)
+	{
+		close(f);
+		return (0);
+	}
+
+	b_r = read(f, b, letters);
+	if (b_r == -1)
+	{
+		close(f);
+		free(b);
+		return (0);
+	}
+
+	b_w = write(STDOUT_FILENO, b, b_r);
+	if (b_w != b_r)
+	{
+		close(f);
+		free(b);
+		return (0);
+	}
+
+	close(f);
 	free(b);
 	return (b_r);
 }
